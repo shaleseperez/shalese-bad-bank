90385186014ad7a550234bb96526b57ff7639490
shaleseperez <127761739+shaleseperez@users.noreply.github.com>
2023-11-20 17:22:37 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders Bad Bank app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Happy Banking/i);
+  const linkElement = screen.getByText(/Get Cash/i);
   expect(linkElement).toBeInTheDocument();
 });
