d6b37af75f1290a27efbcf418e31cea125184e11
shaleseperez <127761739+shaleseperez@users.noreply.github.com>
2023-12-04 14:13:25 -0500

    Add files via upload

--- mongo_test.js
+++ mongo_test.js
@@ -1,8 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
+const uri = "mongodb+srv://sjames8689:myprojectpassword@cluster0.blakbal.mongodb.net/?retryWrites=true&w=majority";
  
 // connect to mongo
-MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+MongoClient.connect(uri, {useUnifiedTopology: true}, function(err, client) {
   console.log("Connected successfully to server");
 
     // database Name
